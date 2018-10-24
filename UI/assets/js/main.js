2d0716c0e6019d587bc885375e88b3a5f2e2ebdf
Seguya-Nicholus <seguyanicholus2015@gmail.com>
2018-10-24 06:53:17 +0300

    added two more functions to enable the admin and attenant to navigate to their shopping cart pages respectively

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -1,46 +1,46 @@
 function myFunction() {
-    var input, filter, table, tr, td, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[5];
-      if (td) {
-        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }       
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[5];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
     }
   }
+}
 
 
   //Below function Executes on click of login button
-function validate(){
-	var username = document.getElementById("username").value;
-	var password = document.getElementById("password").value;
+function validate() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
 
-	if ( username == "admin" && password == "admin"){
-		window.location = "Admin/welcome_admin.html"; //redirecting to other page
-		return false;
+  if (username == "admin" && password == "admin") {
+    window.location = "Admin/welcome_admin.html"; //redirecting to other page
+    return false;
   }
-  else if(username == "attendant" && password == "123"){
+  else if (username == "attendant" && password == "123") {
     window.location = "Attendant/welcome_attendant.html"; //redirecting to other page
-		return false;
+    return false;
 
   }
-	else{
-		attempt --;//Decrementing by one
-		alert("You have left "+attempt+" attempt;");
-		
-		//Disabling fields after 3 attempts
-		if( attempt == 0){
-			document.getElementById("username").disabled = true;
-			document.getElementById("password").disabled = true;
-			document.getElementById("submit").disabled = true;
-			return false;
-		}
-	}
+  else {
+    alert("Wrong user name and password;");
+  }
+}
+  
+
+function navigate_admin() {
+  window.location.href = "add_admin.html";
+}
+
+function navigate_attendant() {
+  window.location.href = "add_attendant.html";
 }
